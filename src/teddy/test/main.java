3169605023087148f3e75e384cfc3b50fc84806f
xuantruong2k <xuantruong2k@outlook.com>
2018-04-27 16:15:39 +0700

    test basic thread

--- src/teddy/test/main.java
+++ src/teddy/test/main.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.util.Random;
 import java.util.Vector;
 
+import teddy.test.thread.*;
+
 public class main {
 	
 	private int _flag;
@@ -13,5 +15,57 @@ public class main {
 		// TODO Auto-generated method stub
 
 		//KeyInfo.getInstance().runTest();
+		
+//		PrintMachine pm = new PrintMachine();
+//		
+//		Thread t1 = new Thread() {
+//			public void run() {
+//				pm.printSync("thread 1", 200);
+//				//pm.print("thread 1 NONE", 20);
+//			}
+//		};
+//		
+//		Thread t2 = new Thread() {
+//			public void run() {
+//				pm.printSync("thread 2", 200);
+//				//pm.print("thread 2 NONE", 40);
+//			}
+//		};
+//		
+//		Thread t3 = new Thread() {
+//			public void run() {
+//				pm.printSync("thread 3", 200);
+//				//pm.print("thread 3 NONE", 30);
+//			}
+//		};
+//		
+//		t1.start();
+//		t2.start();
+//		t3.start();
+		
+		/// test thread in bank account
+		BankAccount ba = new BankAccount("truong", 10000);
+		
+		Thread t4 = new Thread() {
+			public void run() {
+				ba.widthdrawalSync(3000);
+			}
+		};
+		
+		Thread t5 = new Thread() {
+			public void run() {
+				ba.widthdrawalSync(5000);
+			}
+		};
+		
+		Thread t6 = new Thread() {
+			public void run() {
+				ba.widthdrawalSync(6000);
+			}
+		};
+		
+		t4.start();
+		t5.start();
+		t6.start();
 	}
 }
