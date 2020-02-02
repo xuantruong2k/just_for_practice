623fa397ce8e8011f3cb90b62a0f2e504336f4c9
Truong Nguyen <xuantruong2k@outlook.com>
2020-02-02 19:51:41 +0700

    [observer pattern] implement forecast and statistics display for weather station

---
+++ src/designpattern/observerpattern/StatisticsDisplay.java
@@ -0,0 +1,49 @@
+package designpattern.observerpattern;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+
+	private Subject weatherData;
+	private float maxTemp;
+	private float minTemp;
+	private float sumTemp;
+	private int readingCount;
+
+	public StatisticsDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+		maxTemp = 0.0f;
+		minTemp = 200.0f;
+		sumTemp = 0.0f;
+		readingCount = 0;
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		float avgTemp = calculateAverageTemperature();
+		System.out.println("Statistics display - average temperature: " + avgTemp + ", min temp: " + minTemp + ", max temp: " + maxTemp);
+
+	}
+
+	@Override
+	public void update(float pressure, float temperature, float humidity) {
+		// TODO Auto-generated method stub
+		updateTemp(temperature);
+		display();
+	}
+
+	private void updateTemp(float temperature) {
+		readingCount++;
+		sumTemp += temperature;
+		if (maxTemp < temperature) {
+			maxTemp = temperature;
+		}
+		if (minTemp > temperature) {
+			minTemp = temperature;
+		}
+	}
+
+	private float calculateAverageTemperature() {
+		return (sumTemp / readingCount);
+	}
+}
--- src/designpattern/observerpattern/WeatherStation.java
+++ src/designpattern/observerpattern/WeatherStation.java
@@ -8,9 +8,14 @@ public class WeatherStation {
 		WeatherData weatherData = new WeatherData();
 
 		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+		StatisticsDisplay staticticsDisplay = new StatisticsDisplay(weatherData);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
+		// set pressure - temperature - humidity
 		weatherData.setMeasurements(30.4f, 80, 65);
-
+		weatherData.setMeasurements(29.8f, 60, 65);
+		weatherData.setMeasurements(25.0f, 65, 65);
+		weatherData.setMeasurements(40.2f, 72, 65);
 	}
 
 }
