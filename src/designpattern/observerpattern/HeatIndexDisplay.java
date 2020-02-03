4253f05634c8f8dc433fcc0328b7b536020ad2c4
Truong Nguyen <xuantruong2k@outlook.com>
2020-02-03 09:17:54 +0700

    add heat index display

---
+++ src/designpattern/observerpattern/HeatIndexDisplay.java
@@ -0,0 +1,37 @@
+package designpattern.observerpattern;
+
+public class HeatIndexDisplay implements Observer, DisplayElement {
+
+    private Subject weatherData;
+    private float temperature;
+    private float humidity;
+
+    public HeatIndexDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        float heatIdx = computeHeatIndex(temperature, humidity);
+        System.out.println("Heat index display: " + heatIdx);
+    }
+
+    @Override
+    public void update(float pressure, float temperature, float humidity) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+
+        display();
+    }
+
+    private float computeHeatIndex(float t, float rh) {
+        float index = (float) ((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) + (0.00941695 * (t * t))
+                + (0.00728898 * (rh * rh)) + (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh))
+                + (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 * (rh * rh * rh))
+                + (0.00000142721 * (t * t * t * rh)) + (0.000000197483 * (t * rh * rh * rh))
+                - (0.0000000218429 * (t * t * t * rh * rh)) + 0.000000000843296 * (t * t * rh * rh * rh))
+                - (0.0000000000481975 * (t * t * t * rh * rh * rh)));
+        return index;
+    }
+}
--- src/designpattern/observerpattern/WeatherStation.java
+++ src/designpattern/observerpattern/WeatherStation.java
@@ -9,7 +9,8 @@ public class WeatherStation {
 
 		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
 		StatisticsDisplay staticticsDisplay = new StatisticsDisplay(weatherData);
-		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
 		// set pressure - temperature - humidity
 		weatherData.setMeasurements(30.4f, 80, 65);
