037c8d3da3e47ff51f0d347abb05bf607468e29b
Truong Nguyen <xuantruong2k@outlook.com>
2020-02-17 16:59:47 +0700

    [design pattern] implement 'decorator' example

---
+++ src/designpattern/decorator/DarkRoast.java
@@ -0,0 +1,13 @@
+package designpattern.decorator;
+
+public class DarkRoast extends Beverage {
+
+	public DarkRoast() {
+		description = "Dark Roast Coffee";
+	}
+
+	@Override
+	public double cost() {
+		return 0.99;
+	}
+}
