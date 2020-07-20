e9bff1c038449a7ccf7005429873e7f6d95b083e
Truong Nguyen <xuantruong2k@outlook.com>
2020-07-20 21:32:48 +0700

    [udemy multithreading] data race example

---
+++ src/udemy/multithreading/thread/data/race/MainApp.java
@@ -0,0 +1,39 @@
+package udemy.multithreading.thread.data.race;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        SharedClass sharedClass = new SharedClass();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.checkForDataRace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    public static class SharedClass {
+        private int x = 0;
+        private int y = 0;
+
+        public void increment() {
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace() {
+            if (y > x) {
+                System.out.println("y > x - Data race is detected");
+            }
+        }
+    }
+}
