8c1acab4d9537eb46b212c1936fe18db3364e5ff
Truong Nguyen <xuantruong2k@outlook.com>
2020-07-20 09:35:31 +0700

    [udemy multithread] using multi thread for image processing

--- src/udemy/multithreading/thread/image/processing/AppMain.java
+++ src/udemy/multithreading/thread/image/processing/AppMain.java
@@ -4,8 +4,25 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppMain {
+
+    /**
+     * Here is the testing result which I ran on my working desktop
+     * CPU : 6700 4 cores - 8 threads
+     * Ram : 32 GB (2 x 16)
+     *
+     * Number of threads    Processing time
+     *     1                    1102
+     *     2                    712
+     *     4                    492
+     *     6                    325
+     *     8                    260   <- sweet point
+     *     10                   317
+     *     12                   264
+     */
 
     public static final String SOURCE_FILE = "./resources/udemy/multithreading/many-flowers.jpg";
     public static final String DESTINATION_FILE = "./bin/many-flowers.jpg";
@@ -14,14 +31,54 @@ public class AppMain {
         BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
         BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-        recolorSingleThread(originalImage, resultImage);
+        long startTime = System.currentTimeMillis();
+
+//        recolorSingleThread(originalImage, resultImage);
+        int numberOfThreads = 12;
+        recolorMultiThread(originalImage, resultImage, numberOfThreads);
+
+        long endTime = System.currentTimeMillis();
 
         File outputFile = new File(DESTINATION_FILE);
         ImageIO.write(resultImage, "jpg", outputFile);
+
+        System.out.println("Image processing time: " + String.valueOf(endTime - startTime));
     }
 
     public static void recolorSingleThread(BufferedImage originalImage, BufferedImage resultImage) {
         recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    public static void recolorMultiThread(BufferedImage originalImage, BufferedImage resultImage, int numberOfThreads) {
+        List<Thread> threads = new ArrayList<>();
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight() / numberOfThreads;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            final int threadMultiplier = i;
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int leftCorner = 0;
+                    int topCorner = height * threadMultiplier;
+                    recolorImage(originalImage, resultImage, leftCorner, topCorner, width,  height);
+                }
+            });
+
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner, int width, int height) {
@@ -30,14 +87,13 @@ public class AppMain {
                 recolorPixel(originalImage, resultImage, x, y);
             }
         }
-
     }
 
     public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y) {
         int rgb = originalImage.getRGB(x, y);
         int red = getRed(rgb);
-        int green = getRed(rgb);
-        int blue = getRed(rgb);
+        int green = getGreen(rgb);
+        int blue = getBlue(rgb);
 
         int newRed, newGreen, newBlue;
         if (isShapeOfGray(red, green, blue)) {
@@ -57,7 +113,6 @@ public class AppMain {
     public static void setRGB(BufferedImage image, int x, int y, int rgb) {
         image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
     }
-
 
     public static boolean isShapeOfGray(int red, int green, int blue) {
         int grayVal = 30;
